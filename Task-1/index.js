3bf2c629adf43add6d876890f3f354ec5cbddbeb
SarthakNaik2003 <131641741+SarthakNaik2003@users.noreply.github.com>
2024-05-15 13:12:26 +0530

    task-2 done Function Component created

--- Task-1/index.js
+++ Task-1/index.js
@@ -1,1 +1,11 @@
-ReactDOM.render(<p>hello this is sarthak naik welcome </p>, document.getElementById("root"))
+ReactDOM.render(
+    <p>
+        <p>hello this is sarthak naik welcome </p>
+        {/* task2 adding ul in root  */}
+        <ul>
+            <li>Smart</li>
+            <li>Hardworking</li>
+            <li>Suppotive</li>
+        </ul>
+    </p>,
+    document.getElementById("root"))
